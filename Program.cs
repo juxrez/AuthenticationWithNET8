993c3c2b2af1731bdd6f0b4f9f6513f771606d15
Alexis Juarez Nava <Alexis.Juarez.Nava.-ND@disney.com>
2023-10-06 15:07:28 -0600

    Add comments to program.cs

--- Program.cs
+++ Program.cs
@@ -7,12 +7,16 @@ using Swashbuckle.AspNetCore.Filters;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Add DBContext where the Identity tables are going to be created.
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+//Add Swagger bearer token authentication 
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
@@ -32,7 +36,7 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>()
 
 var app = builder.Build();
 
-//NET 8 Magic
+//NET 8 mapping of Identity tables
 app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
